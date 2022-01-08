c3669a54b9c871fbe0ba316cb61e2fad5cc7fd6f
ArtemZh <zhkhvartjom@rambler.ru>
2022-01-08 18:53:03 +0300

    Test1

--- src/Otus.Teaching.PromoCodeFactory.WebHost/Program.cs
+++ src/Otus.Teaching.PromoCodeFactory.WebHost/Program.cs
@@ -15,7 +15,7 @@ namespace Otus.Teaching.PromoCodeFactory.WebHost
         {
             CreateHostBuilder(args).Build().Run();
         }
-
+         
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
